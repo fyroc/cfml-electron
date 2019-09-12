2897c505ca1ae873c635a1ac3461630c3d891e8e
“Cory <me@coryfail.com>
2019-09-11 20:32:08 -0400

    Update to latest Electron

--- main.js
+++ main.js
@@ -1,68 +1,89 @@
-
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
-const is = require("electron-is");
+const {
+    app,
+    BrowserWindow
+} = require('electron')
+const is = require("electron-is")
+const exec = require('child_process').exec
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  stopLucee();
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
-});
+let win
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
-  // Start Lucee
-  startLucee();
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600+410});
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+    startLucee()
+    createWindow()
+})
 
-  // Wait a few seconds so Lucee can load
-  setTimeout(function() {
-      mainWindow.loadUrl('http://localhost:8888/')
-  }, 5000);
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
 
-  // Open the DevTools.
-  mainWindow.openDevTools();
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
+})
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    stopLucee();
-    mainWindow = null;
-  });
-});
+app.on('before-quit', () => {
+    stopLucee()
+})
+
+function createWindow() {
+
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    // and load the index.html of the app.
+    setTimeout(function() {
+        win.loadURL('http://localhost:8888/')
+    }, 5000);
+
+    // Open the DevTools.
+    win.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+    })
+}
 
 function startLucee() {
-  const process = require('child_process');
+    var cmd = (is.windows()) ? 'lucee/startup.bat' : './lucee/startup.sh';
 
-  var cmd = (is.windows()) ? 'lucee/startup.bat' : './lucee/startup.sh';
-      
-  var child = process.spawn(cmd); 
-
-  child.on('close', function (code) {
-
-  });
-};
+    execute(cmd, (output) => {
+        console.log(output)
+    })
+}
 
 function stopLucee() {
-  const process = require('child_process');
+    var cmd = (is.windows()) ? 'lucee/shutdown.bat' : './lucee/shutdown.sh'
 
-  var cmd = (is.windows()) ? 'lucee/shutdown.bat' : './lucee/shutdown.sh';
-      
-  var child = process.spawn(cmd); 
+    execute(cmd, (output) => {
+        console.log(output)
+    })
+}
 
-  child.on('close', function (code) {
-
-  });
-};
+function execute(command, callback) {
+    exec(command, (error, stdout, stderr) => { 
+        callback(stdout)
+    })
+}
